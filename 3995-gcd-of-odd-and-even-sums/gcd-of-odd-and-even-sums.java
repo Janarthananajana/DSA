191d698fb4934b81ada5c05a5c4845033ea9728f
Janarthananajana <145571868+Janarthananajana@users.noreply.github.com>
2026-07-15 20:49:34 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (93.05%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -1,27 +1,5 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        // int a =0;
-        // int b =0;
-        // for(int i=1;i<=n*2;i++)
-        // {
-        //     if(i%2!=0) a+=i;
-        //     else b+=i;
-        // }
-        // int min = Math.min(a, b);
-        // int gcd = 1;
-
-        // for (int i = 1; i * i <= min; i++) {
-        //     if (a % i == 0 && b % i == 0) {
-        //         gcd = Math.max(gcd, i);
-
-        //         int other = min / i;
-
-        //         if (a % other == 0 && b % other == 0) {
-        //             gcd = Math.max(gcd, other);
-        //         }
-        //     }
-        // }
-
-        return n;
+          return n;
     }
 }
