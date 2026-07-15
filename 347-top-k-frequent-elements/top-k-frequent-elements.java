3c1cc40ed2dd2dfa68c44cba956a1b047de42540
Janarthananajana <145571868+Janarthananajana@users.noreply.github.com>
2026-07-15 23:27:28 +0530

    Time: 16 ms (39.25%) | Memory: 47.8 MB (19.72%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -5,14 +5,12 @@ class Solution {
         for (int num : nums) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
-
-        // Step 2: Convert map to list of entries
         List<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
 
-        // Step 3: Sort by frequency (descending)
+        
         Collections.sort(list, (a, b) -> Integer.compare(b.getValue(), a.getValue()));
 
-        // Step 4: Store first k keys
+     
         int[] ans = new int[k];
 
         for (int i = 0; i < k; i++) {
