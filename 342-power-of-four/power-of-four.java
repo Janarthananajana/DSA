094d32433c2f2168e12b47cd1a1a631d70e1ccd7
Janarthananajana <145571868+Janarthananajana@users.noreply.github.com>
2026-07-16 21:37:42 +0530

    Time: 7 ms (0.50%) | Memory: 42.7 MB (33.62%) - LeetSync

---
+++ 342-power-of-four/power-of-four.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n==1) return true;
+        if(n>=4)
+        {
+            for(int i=0;i<=Math.sqrt(n);i++)
+            {
+                if(Math.pow(4,i)==n) return true;
+            }
+        }
+        return false;
+    }
+}
