47245ff938656b391d32648149854b15fd83f652
Janarthananajana <145571868+Janarthananajana@users.noreply.github.com>
2026-07-19 22:45:36 +0530

    Time: 2 ms (87.77%) | Memory: 46.5 MB (25.54%) - LeetSync

---
+++ 983-validate-stack-sequences/validate-stack-sequences.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> st = new Stack<>();
+       int index =0;
+       for(int a:pushed)
+       {
+           st.push(a);
+           while(!st.isEmpty() && st.peek()==popped[index])
+           {
+               st.pop();
+               index++;
+           }
+
+       }
+       return st.isEmpty();
+    }
+}
